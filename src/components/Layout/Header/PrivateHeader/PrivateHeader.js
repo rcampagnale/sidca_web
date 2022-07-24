faf77f09fbdce194b796e2f7d63e7d0871824dfc
Francisco Lozano <mellifran1@gmail.com>
2022-07-24 19:20:21 -0300

    añade carrousel a home

--- src/components/Layout/Header/PrivateHeader/PrivateHeader.js
+++ src/components/Layout/Header/PrivateHeader/PrivateHeader.js
@@ -20,13 +20,13 @@ const PrivateHeader = () => {
                 <img className={styles.headerLogo} src={logo} alt="SiDCa logo" />
             </a>
             <ul className={styles.headerNav}>
-                {/* <li onClick={() => history.push('/capacitaciones')}>Capacitaciones</li>
-                <li onClick={() => history.push('/casa-del-docente')}>Casa del Docente</li> */}
+                <li onClick={() => history.push('/capacitaciones')}>Capacitaciones</li>
+                <li onClick={() => history.push('/casa-del-docente')}>Casa del Docente</li>
                 <li onClick={() => history.push('/contacto')}>Contacto</li>
                 <li onClick={() => history.push('/nosotros')}>Nosotros</li>
-                {/* <li onClick={() => history.push('/novedades')}>Novedades</li>
+                <li onClick={() => history.push('/novedades')}>Novedades</li>
                 <li onClick={() => history.push('/predio')}>Predio</li>
-                <li onClick={() => history.push('/turismo')}>Turismo</li> */}
+                <li onClick={() => history.push('/turismo')}>Turismo</li>
                 {/* {user.profue.cotizante && } */}
                 <li onClick={() => history.push('/cuotas')}>Cuotas</li>
             </ul>
