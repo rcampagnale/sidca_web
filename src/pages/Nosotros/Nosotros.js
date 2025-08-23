ea0d247e9a5bfd9ea6d9d75c22e8e57da442c286
Tu Nombre Real <tucorreo@dominio.com>
2025-08-23 18:20:12 -0300

    actualizamos página nosotros

--- src/pages/Nosotros/Nosotros.js
+++ src/pages/Nosotros/Nosotros.js
@@ -1,57 +1,96 @@
-import React from 'react';
-import styles from './styles.module.css';
-import { Button } from 'primereact/button';
-import { useHistory } from 'react-router-dom';
-import ImageSlider from '../../components/Slider/ImageSlider';
+import React from "react";
+import styles from "./styles.module.css";
+import { Button } from "primereact/button";
+import { useHistory } from "react-router-dom";
+import ImageSlider from "../../components/Slider/ImageSlider";
 
-import img1 from '../../assets/images/nosotros/nosotros-01.jpeg';
-import img2 from '../../assets/images/nosotros/nosotros-02.jpeg';
-import img3 from '../../assets/images/nosotros/nosotros-03.jpeg';
-import img4 from '../../assets/images/nosotros/nosotros-04.jpeg';
-import img5 from '../../assets/images/nosotros/nosotros-05.jpeg';
-import img6 from '../../assets/images/nosotros/nosotros-06.jpeg';
-import img7 from '../../assets/images/nosotros/nosotros-07.jpeg';
-import img8 from '../../assets/images/nosotros/nosotros-08.jpeg';
-import img9 from '../../assets/images/nosotros/nosotros-09.jpeg';
-import img10 from '../../assets/images/nosotros/nosotros-10.jpeg';
-import img11 from '../../assets/images/nosotros/nosotros-11.jpeg';
-import img12 from '../../assets/images/nosotros/nosotros-12.jpeg';
+import img1 from "../../assets/nosotros/somos1.jpg";
+import img2 from "../../assets/nosotros/somos2.jpg";
+import img3 from "../../assets/nosotros/somos3.jpg";
+import img4 from "../../assets/nosotros/somos4.jpg";
+import img5 from "../../assets/nosotros/somos5.jpg";
+import img6 from "../../assets/nosotros/somos6.jpg";
+import img7 from "../../assets/nosotros/somos7.jpg";
+import img8 from "../../assets/nosotros/somos8.jpg";
+import img9 from "../../assets/nosotros/somos9.jpg";
+import img10 from "../../assets/nosotros/somos10.jpg";
+import img11 from "../../assets/nosotros/somos11.png";
+import img12 from "../../assets/nosotros/somos12.png";
 
 const Nosotros = () => {
+  const slides = [
+    { url: img1, title: "img1-somos" },
+    { url: img2, title: "img2-somos" },
+    { url: img3, title: "img3-somos" },
+    { url: img4, title: "img4-somos" },
+    { url: img5, title: "img5-somos" },
+    { url: img6, title: "img6-somos" },
+    { url: img7, title: "img7-somos" },
+    { url: img8, title: "img8-somos" },
+    { url: img9, title: "img9-somos" },
+    { url: img10, title: "img10-somos" },
+    { url: img11, title: "img11-somos" },
+    { url: img12, title: "img12-somos" },
+  ];
 
-    const slides = [
-        { url: img1, title: "img1-nosotros" },
-        { url: img2, title: "img2-nosotros" },
-        { url: img3, title: "img3-nosotros" },
-        { url: img4, title: "img4-nosotros" },
-        { url: img5, title: "img5-nosotros" },
-        { url: img6, title: "img6-nosotros" },
-        { url: img7, title: "img7-nosotros" },
-        { url: img8, title: "img8-nosotros" },
-        { url: img9, title: "img9-nosotros" },
-        { url: img10, title: "img10-nosotros" },
-        { url: img11, title: "img11-nosotros" },
-        { url: img12, title: "img12-nosotros" },
-    ];
+  const history = useHistory();
 
-    const history = useHistory();
+  return (
+    <div className={styles.componentContainer}>
+      {/* Título y descripción */}
+      <h1 className={styles.title}>Quiénes Somos</h1>
+      <p className={styles.text}>
+        SIDCA, el Sindicato de Docentes de Catamarca, inscripción gremial 2902,
+        adherido a la Confederación Argentina de Educadores, CEA, Personería
+        gremial 1716.
+        <br />
+        <br />
+        Trabaja en la firme defensa de los derechos docentes, reafirmando el
+        reclamo permanente por mejores condiciones en el desempeño profesional,
+        promoviendo la capacitación y el perfeccionamiento, exigiendo el respeto
+        a normativas justas para los trabajadores y trabajadoras de la
+        educación.
+        <br />
+        <br />
+        Creemos que la educación es motor indiscutible de desarrollo de los
+        pueblos y por eso, defendemos las mejores condiciones laborales para
+        quienes la imparten, en consonancia con las instituciones gremiales
+        consolidadas a nivel país y la Internacional de la Educación, que cuidan
+        y protegen a educadores del país y el mundo.
+      </p>
 
-    return (
-        <div className={styles.componentContainer}>
-            <div className={styles.btnSection}>
-                <Button icon="pi pi-arrow-circle-right" label="Turismo" className="p-button-raised p-button-warning" onClick={() => history.push('/turismo')} />
-                <Button icon="pi pi-arrow-circle-right" label="Casa del docente" className="p-button-raised p-button-warning" onClick={() => history.push('/casa-del-docente')} />
-                <Button icon="pi pi-arrow-circle-right" label="Predio" className="p-button-raised p-button-warning" onClick={() => history.push('/predio')} />
-            </div>
-            <div className={styles.title}>Quienes Somos</div>
-            <p className={styles.text}>SIDCA, el Sindicato de Docentes de Catamarca, inscripción gremial 2902, adherido a la Confederación Argentina de Educadores, CEA, Personería gremial 1716.
-                <br /><br />Trabaja en la firme defensa de los derechos docentes, reafirmando el reclamo permanente por mejores condiciones en el desempeño profesional, promoviendo la capacitación y el perfeccionamiento, exigiendo el respeto a normativas justas para los trabajadores y trabajadoras de la educación.
-                <br /><br />Creemos que la educación es motor indiscutible de desarrollo de los pueblos y por eso, defendemos las mejores condiciones laborales para quienes la imparten, en consonancia con las instituciones gremiales consolidadas a nivel país y la Internacional de la Educación, que cuidan y protegen a educadores del país y el mundo.</p>
-            <div className={styles.containerSlides}>
-                <ImageSlider slides={slides} />
-            </div>
-        </div>
-    )
-}
+      {/* Botones abajo del texto */}
+      <div className={styles.btnSection}>
+        <Button
+          icon="pi pi-send"
+          /* Ícono de viajes */
+          label="Turismo"
+          className="p-button-raised p-button-warning"
+          onClick={() => history.push("/turismo")}
+        />
+        <Button
+          icon="pi pi-home"
+          /* Ícono de casa */
+          label="Casa del docente"
+          className="p-button-raised p-button-warning"
+          onClick={() => history.push("/casa-del-docente")}
+        />
+        <Button
+          icon="pi pi-map"
+          /* Ícono de predio */
+          label="Predio"
+          className="p-button-raised p-button-warning"
+          onClick={() => history.push("/predio")}
+        />
+      </div>
 
-export default Nosotros
+      {/* Carrusel */}
+      <div className={styles.containerSlides}>
+        <ImageSlider slides={slides} />
+      </div>
+    </div>
+  );
+};
+
+export default Nosotros;
+
